a7e844494de7b3a943059de052e2157363a189b4
zikju <zqzer@zq.lt>
2022-04-27 16:59:13 +0300

    update(UserController.php): add create() method, that handles user creation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\User\CreateRequest;
+use App\Models\User;
+
 class UserController extends Controller
 {
-    //
+    /**
+     * Creates new user
+     *
+     * @param CreateRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create (CreateRequest $request)
+    {
+        // Retrieve the validated input data...
+        $validatedRequestData = $request->validated();
+
+        $userData = User::query()->create(array_merge(
+            $validatedRequestData,
+            ['password' => bcrypt($request->password)]
+        ));
+
+        return response()->json($userData);
+    }
 }

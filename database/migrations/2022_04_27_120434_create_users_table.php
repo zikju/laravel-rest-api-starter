edb9c5265bdbad89ee36396593db3b12e7f920cd
zikju <zqzer@zq.lt>
2022-04-27 15:08:37 +0300

    Add new migration, that creates 'Users' table

---
+++ database/migrations/2022_04_27_120434_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email', 100)->unique();
+            $table->string('password', 100);
+            $table->enum('role', ['user', 'admin'])->default('user');
+            $table->enum('status', ['pending', 'active', 'disabled'])->default('pending');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};

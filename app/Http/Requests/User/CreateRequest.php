ea92fd2995a3193a0d3c99ed9dac583b18f6e2bd
zikju <zqzer@zq.lt>
2022-04-27 21:59:56 +0300

    refactor(User/CreateRequest): add return types to methods

--- app/Http/Requests/User/CreateRequest.php
+++ app/Http/Requests/User/CreateRequest.php
@@ -12,7 +12,7 @@ class CreateRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -22,7 +22,7 @@ class CreateRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'email' => 'required|string|email|max:100|unique:users',

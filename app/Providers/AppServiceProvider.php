7a50c34e3f1073466d01a078c1cdb3743ffacdb8
zikju <zqzer@zq.lt>
2022-04-27 14:57:08 +0300

    Add 'Sanctum::ignoreMigrations' method.
    We will not use Sanctum's default migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // WORKAROUND: Do not use Sanctum's default migrations
+        Sanctum::ignoreMigrations();
     }
 
     /**

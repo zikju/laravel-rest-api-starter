a557ebc448fb5b99c11e1a863100d4f0eb9e6c05
zikju <zqzer@zq.lt>
2022-05-01 20:39:00 +0300

    Feat(middleware): add middleware that checks tokens headers in request

---
+++ app/Http/Middleware/EnsureTokenHeadersAreExist.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Str;
+
+class EnsureTokenHeadersAreExist
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return JsonResponse|mixed
+     */
+    public function handle(Request $request, Closure $next): mixed
+    {
+        // Verify if 'Access Token' exist in request header
+        $accessToken = $request->bearerToken();
+        if (! $accessToken) {
+            return response()->json([
+                'status' => 'error',
+                'message'=> 'INVALID_ACCESS_TOKEN'
+            ], 400);
+        }
+
+        // Verify if 'Refresh Token' exist in request header
+        $refreshTokenHeaderKey = env('JWT_REFRESH_TOKEN_HEADER_KEY', 'X-REFRESH-TOKEN-ID');
+        $refreshToken = $request->header($refreshTokenHeaderKey);
+        if (! $refreshToken || ! Str::isUuid($refreshToken)) {
+            return response()->json([
+                'status' => 'error',
+                'message'=> 'INVALID_REFRESH_TOKEN'
+            ], 400);
+        }
+
+        return $next($request);
+    }
+
+}

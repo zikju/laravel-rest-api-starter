40527826ab9147388b394c85e36855db17d0efa5
zikju <zqzer@zq.lt>
2022-05-02 21:52:42 +0300

    Refactor: add property

--- app/Http/Requests/User/CreateRequest.php
+++ app/Http/Requests/User/CreateRequest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateRequest extends FormRequest
 {
+    public mixed $password;
 
     /**
      * Determine if the user is authorized to make this request.
--- app/Http/Requests/User/DeleteRequest.php
+++ app/Http/Requests/User/DeleteRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DeleteRequest extends FormRequest
 {
+    public mixed $id;
+
     /**
      * Determine if the user is authorized to make this request.
      *

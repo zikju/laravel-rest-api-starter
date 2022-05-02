5d537ceefdd06ef23f162a2d0de964a1ca17a9d2
zikju <zqzer@zq.lt>
2022-05-02 12:29:22 +0300

    feat(middleware): add ForceJsonResponse.php middleware
    Force Laravel to return JSON responses

---
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ForceJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        return $next($request);
+    }
+}
